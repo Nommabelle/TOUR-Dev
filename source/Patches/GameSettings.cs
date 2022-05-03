81aee666eb0d3b1c076b76d8ba39685022987adc
ItsTheNumberH <h-newton@hotmail.com>
2022-05-03 15:22:41 +0100

    Lobby settings menu now all in one. No Tab press needed

--- source/Patches/GameSettings.cs
+++ source/Patches/GameSettings.cs
@@ -16,55 +16,7 @@ namespace TownOfUs
         public static bool AllOptions;
         public static bool LastTab;
 
-        /*public static string StringBuild()
-        {
-            var builder = new StringBuilder("Roles:\n");
-            foreach (var option in TownOfUs.Roles)
-            {
-                builder.AppendLine($"     {option.Name}: {option}");
-            }
-
-            builder.AppendLine("Modifiers:");
-            foreach (var option in TownOfUs.Modifiers)
-            {
-                builder.AppendLine($"     {option.Name}: {option}");
-            }
-            
-            
-            foreach (var option in TownOfUs.AllOptions)
-            {
-                builder.AppendLine($"{option.Name}: {option}");
-            }
-            
-
-            return builder.ToString();
-        }
-
-        [HarmonyPatch(typeof(LobbyBehaviour), nameof(LobbyBehaviour.FixedUpdate))]
-        public static class LobbyFix
-        {
-
-            public static bool Prefix()
-            {
-                
-                DestroyableSingleton<HudManager>.Instance.GameSettings.text = StringBuild();
-                DestroyableSingleton<HudManager>.Instance.GameSettings.gameObject.SetActive(true);
-                return false;
-            }
-        }
-
-
-        [HarmonyPatch(typeof(HudManager), nameof(HudManager.Update))]
-        [HarmonyAfter("com.comando.essentials")]
-        public static class FixScale
-        {
-            public static void Prefix(HudManager __instance)
-            {
-//                __instance.GameSettings.scale = 0.3f;
-            }
-        }*/
-
-        [HarmonyPatch] //ToHudString
+        [HarmonyPatch]
         private static class GameOptionsDataPatch
         {
             public static IEnumerable<MethodBase> TargetMethods()
@@ -78,35 +30,20 @@ namespace TownOfUs
 
                 foreach (var option in CustomOption.CustomOption.AllOptions)
                 {
-                    if (option.Name == "Custom Game Settings" && !AllOptions) break;
+                    if (option.Name == "Crewmate Investigative Roles") {
+                        builder.Append("(Scroll for all settings)");
+                    } else if (option.Name == "Custom Game Settings") {
+                        builder.AppendLine("");
+                        builder.Append(new StringBuilder(__result));
+                    }
                     if (option.Type == CustomOptionType.Button) continue;
                     if (option.Type == CustomOptionType.Header) builder.AppendLine($"\n{option.Name}");
                     else if (option.Indent) builder.AppendLine($"     {option.Name}: {option}");
                     else builder.AppendLine($"{option.Name}: {option}");
                 }
 
-
                 __result = builder.ToString();
-
-
-                if (CustomOption.CustomOption.LobbyTextScroller && AllOptions)
-                    __result = __result.Insert(__result.IndexOf('\n'), " (Scroll for more)");
-                else __result = __result.Insert(__result.IndexOf('\n'), "(Press Tab to see all options)");
-
-
                 __result = $"<size=1.25>{__result}</size>";
-            }
-        }
-
-        [HarmonyPatch(typeof(LobbyBehaviour), nameof(LobbyBehaviour.FixedUpdate))]
-        private static class LobbyBehaviourPatch
-        {
-            private static void Postfix()
-            {
-                if (LastTab && !Input.GetKeyInt(KeyCode.Tab)) AllOptions = !AllOptions;
-                LastTab = Input.GetKeyInt(KeyCode.Tab);
-
-                //                HudManager.Instance.GameSettings.scale = 0.5f;
             }
         }
 
@@ -115,7 +52,7 @@ namespace TownOfUs
         {
             public static void Postfix(ref GameOptionsMenu __instance)
             {
-                __instance.GetComponentInParent<Scroller>().ContentYBounds.max = 113f;
+                __instance.GetComponentInParent<Scroller>().ContentYBounds.max = (__instance.Children.Length - 6.5f) / 2;
             }
         }
     }
