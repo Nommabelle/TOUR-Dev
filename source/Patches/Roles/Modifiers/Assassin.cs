689034c5debc724038720a3a2fbdeaf9b408309d
ItsTheNumberH <h-newton@hotmail.com>
2022-05-09 13:35:20 +0100

    Updated comment

--- source/Patches/Roles/Modifiers/Assassin.cs
+++ source/Patches/Roles/Modifiers/Assassin.cs
@@ -70,7 +70,7 @@ namespace TownOfUs.Roles.Modifiers
 
             // Add vanilla crewmate if enabled
             if (CustomGameOptions.AssassinCrewmateGuess) ColorMapping.Add("Crewmate", Colors.Crewmate);
-            //Add bait if enabled
+            //Add modifiers if enabled
             if (CustomGameOptions.AssassinGuessModifiers && CustomGameOptions.BaitOn > 0) ColorMapping.Add("Bait", Colors.Bait);
             if (CustomGameOptions.AssassinGuessModifiers && CustomGameOptions.DiseasedOn > 0) ColorMapping.Add("Diseased", Colors.Diseased);
             if (CustomGameOptions.AssassinGuessModifiers && CustomGameOptions.TorchOn > 0) ColorMapping.Add("Torch", Colors.Torch);
