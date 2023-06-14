fe041f4bb1d6ce6ed8ba17348b2245126e383935
MyDragonBreath <38029594+MyDragonBreath@users.noreply.github.com>
2023-06-14 21:41:33 +1000

    reactor text move

--- source/Patches/LogoPatch.cs
+++ source/Patches/LogoPatch.cs
@@ -19,10 +19,11 @@ namespace TownOfUs {
             position.DistanceFromEdge = new Vector3(-0.2f, 1f, 10f);
             position.Alignment = AspectPosition.EdgeAlignments.Top;
 
-            position.StartCoroutine(Effects.Lerp(0.1f, new System.Action<float>((p) => {
+            position.StartCoroutine(Effects.Lerp(0.1f, new System.Action<float>((p) =>
+            {
                 position.AdjustPosition();
             })));
-            
+
 
             var scaler = touLogo.AddComponent<AspectScaledAsset>();
             var renderers = new Il2CppSystem.Collections.Generic.List<SpriteRenderer>();
@@ -33,6 +34,19 @@ namespace TownOfUs {
 
             touLogo.transform.SetParent(GameObject.Find("RightPanel").transform);
 
+
+            var reactor = GameObject.Find("LeftPanel").transform.GetChild(3);
+            reactor.SetParent(GameObject.Find("RightPanel").transform);
+            reactor.localPosition = new Vector3(7.5887f, - 3f, 0f);
+
+            var rpos = reactor.GetComponent<AspectPosition>();
+            rpos.Alignment = AspectPosition.EdgeAlignments.LeftBottom;
+            rpos.DistanceFromEdge = new Vector3(13f, 0f, 10f);
+
+
+            rpos.StartCoroutine(Effects.Lerp(0.1f, new System.Action<float>((p) => {
+                rpos.AdjustPosition();
+            })));
         }
     }
 }
