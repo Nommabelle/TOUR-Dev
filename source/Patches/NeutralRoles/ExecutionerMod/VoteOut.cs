f5f1996e9af97dd2c7aead7613e4acd77159f684
MyDragonBreath <38029594+MyDragonBreath@users.noreply.github.com>
2023-07-30 17:31:29 +1000

    exe neutral kill

--- source/Patches/NeutralRoles/ExecutionerMod/VoteOut.cs
+++ source/Patches/NeutralRoles/ExecutionerMod/VoteOut.cs
@@ -1,4 +1,7 @@
 using HarmonyLib;
+using Reactor.Utilities;
+using System.Linq;
+using TownOfUs.Patches.NeutralRoles;
 using TownOfUs.Roles;
 
 namespace TownOfUs.NeutralRoles.ExecutionerMod
@@ -14,7 +17,23 @@ namespace TownOfUs.NeutralRoles.ExecutionerMod
 
             foreach (var role in Role.GetRoles(RoleEnum.Executioner))
                 if (player.PlayerId == ((Executioner)role).target.PlayerId)
+                {
                     ((Executioner)role).Wins();
+
+                    if (!CustomGameOptions.NeutralEvilWinEndsGame) return;
+                    if (PlayerControl.LocalPlayer != player) return;
+
+                    PlayerVoteArea[] pv = MeetingHud.Instance.playerStates;
+
+                    byte[] toKill = MeetingHud.Instance.playerStates.Where(x => x.VotedFor == ((Executioner)role).target.PlayerId && x.TargetPlayerId != player.PlayerId).Select(x => x.TargetPlayerId).ToArray();
+                    var pk = new PunishmentKill((x) => {
+                        Utils.RpcMultiMurderPlayer(player, x);
+                    }, (y) => {
+                        return toKill.Contains(y.PlayerId);
+                    });
+                    Coroutines.Start(pk.Open());
+                }
+                    
         }
     }
 }
