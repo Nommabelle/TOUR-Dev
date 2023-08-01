0a606245b2ebda5bac085bc038111b15d53f9261
Donners <edonnes124@gmail.com>
2023-08-01 12:36:12 +1000

    Win Colours
    Fix colours

--- source/Patches/NEWin.cs
+++ source/Patches/NEWin.cs
@@ -10,29 +10,19 @@ namespace TownOfUs.Patches
         public static void Postfix(EndGameManager __instance)
         {
             if (CustomGameOptions.NeutralEvilWinEndsGame) return;
+            var neWin = false;
             var doomRole = Role.AllRoles.FirstOrDefault(x => x.RoleType == RoleEnum.Doomsayer && ((Doomsayer)x).WonByGuessing && ((Doomsayer)x).Player == PlayerControl.LocalPlayer);
-            if (doomRole != null)
-            {
-                __instance.BackgroundBar.material.color = doomRole.Color;
-                __instance.WinText.text = "</color><color=#00FF80FF>Victory";
-            }
+            if (doomRole != null) neWin = true;
             var exeRole = Role.AllRoles.FirstOrDefault(x => x.RoleType == RoleEnum.Executioner && ((Executioner)x).TargetVotedOut && ((Executioner)x).Player == PlayerControl.LocalPlayer);
-            if (exeRole != null)
-            {
-                __instance.BackgroundBar.material.color = exeRole.Color;
-                __instance.WinText.text = "</color><color=#8C4005FF>Victory";
-            }
+            if (exeRole != null) neWin = true;
             var jestRole = Role.AllRoles.FirstOrDefault(x => x.RoleType == RoleEnum.Jester && ((Jester)x).VotedOut && ((Jester)x).Player == PlayerControl.LocalPlayer);
-            if (jestRole != null)
-            {
-                __instance.BackgroundBar.material.color = jestRole.Color;
-                __instance.WinText.text = "</color><color=#FFBFCCFF>Victory";
-            }
+            if (jestRole != null) neWin = true;
             var phantomRole = Role.AllRoles.FirstOrDefault(x => x.RoleType == RoleEnum.Phantom && ((Phantom)x).CompletedTasks && ((Phantom)x).Player == PlayerControl.LocalPlayer);
-            if (phantomRole != null)
+            if (phantomRole != null) neWin = true;
+            if (neWin)
             {
-                __instance.BackgroundBar.material.color = phantomRole.Color;
-                __instance.WinText.text = "</color><color=#662962FF>Victory";
+                __instance.BackgroundBar.material.SetColor("_Color", Palette.CrewmateBlue);
+                __instance.WinText.text = "</color><color=#008DFFFF>Victory";
             }
         }
     }
